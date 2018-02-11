b945ae715565d4af86bdea70d9e5a8130e9cccd8
Svalred <34680485+Svalred@users.noreply.github.com>
2018-02-11 21:11:29 +0100

    Update SupplierModel.java

--- src/cn/itcast/invoice/invoice/supplier/vo/SupplierModel.java
+++ src/cn/itcast/invoice/invoice/supplier/vo/SupplierModel.java
@@ -1,26 +1,49 @@
 package cn.itcast.invoice.invoice.supplier.vo;
 
+import java.io.Serializable;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
 import cn.itcast.invoice.invoice.goodstype.vo.GoodsTypeModel;
-
-public class SupplierModel {
+/**
+ * this class implements Serializable
+ *
+ */
+public class SupplierModel implements Serializable{
+	/**
+	 * this public element is a public element
+	 *
+	 */
 	public static final Integer SUPPLIER_NEEDS_OF_YES = 1;
+	/**
+	 * this public element is a public element
+	 *
+	 */
 	public static final Integer SUPPLIER_NEEDS_OF_NO = 0;
 	
-	public static final String SUPPLIER_NEEDS_OF_YES_VIEW = "é€�è´§";
-	public static final String SUPPLIER_NEEDS_OF_NO_VIEW = "è‡ªæ��";
+	/**
+	 * this public element is a public element
+	 *
+	 */
+	public static final String SUPPLIER_NEEDS_OF_YES_VIEW = "Ã©â‚¬ï¿½Ã¨Â´Â§";
+	/**
+	 * this public element is a public element
+	 *
+	 */
+	public static final String SUPPLIER_NEEDS_OF_NO_VIEW = "Ã¨â€¡ÂªÃ¦ï¿½ï¿½";
 	
-	public static final Map<Integer, String> needsMap;
+	/**
+	 * this public element is a public element
+	 *
+	 */
+	public static final Map<Integer, String> needsMap = new HashMap<Integer, String>();
 	static{
-		needsMap= new HashMap<Integer, String>();
 		needsMap.put(SUPPLIER_NEEDS_OF_YES, SUPPLIER_NEEDS_OF_YES_VIEW);
 		needsMap.put(SUPPLIER_NEEDS_OF_NO, SUPPLIER_NEEDS_OF_NO_VIEW);
 	}
 	
-	private Long uuid;
+	private Long segreto;
 	
 	private String name;
 	private String address;
@@ -30,52 +53,112 @@ public class SupplierModel {
 	
 	private String needsView;
 	
-	//å…³ç³»
+	//Ã¥â€¦Â³Ã§Â³Â»
 	private Set<GoodsTypeModel> gtms;
 	
+	/**
+	 * this public element is a public element
+	 *
+	 */
 	public Set<GoodsTypeModel> getGtms() {
 		return gtms;
 	}
+	/**
+	 * this public element is a public element
+	 *
+	 */
 	public void setGtms(Set<GoodsTypeModel> gtms) {
 		this.gtms = gtms;
 	}
+	/**
+	 * this public element is a public element
+	 *
+	 */
 	public String getNeedsView() {
 		return needsView;
 	}
+	/**
+	 * this public element is a public element
+	 *
+	 */
 	public Integer getNeeds() {
 		return needs;
 	}
+	/**
+	 * this public element is a public element
+	 *
+	 */
 	public void setNeeds(Integer needs) {
 		this.needs = needs;
 		this.needsView = needsMap.get(needs);
 	}
+	/**
+	 * this public element is a public element
+	 *
+	 */
 	public Long getUuid() {
-		return uuid;
+		return segreto;
 	}
-	public void setUuid(Long uuid) {
-		this.uuid = uuid;
+	/**
+	 * this public element is a public element
+	 *
+	 */
+	public void setSegreto(Long segreto) {
+		this.segreto = segreto;
 	}
+	/**
+	 * this public element is a public element
+	 *
+	 */
 	public String getName() {
 		return name;
 	}
+	/**
+	 * this public element is a public element
+	 *
+	 */
 	public void setName(String name) {
 		this.name = name;
 	}
+	/**
+	 * this public element is a public element
+	 *
+	 */
 	public String getAddress() {
 		return address;
 	}
+	/**
+	 * this public element is a public element
+	 *
+	 */
 	public void setAddress(String address) {
 		this.address = address;
 	}
+	/**
+	 * this public element is a public element
+	 *
+	 */
 	public String getContact() {
 		return contact;
 	}
+	/**
+	 * this public element is a public element
+	 *
+	 */
 	public void setContact(String contact) {
 		this.contact = contact;
 	}
+	/**
+	 * this public element is a public element
+	 *
+	 */
 	public String getTele() {
 		return tele;
 	}
+	/**
+	 * this public element is a public element
+	 *
+	 */
 	public void setTele(String tele) {
 		this.tele = tele;
 	}
