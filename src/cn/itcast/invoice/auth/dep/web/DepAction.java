ee5820f59c76a63d7e4e210dfc1b7702ace70b9d
Svalred <34680485+Svalred@users.noreply.github.com>
2018-02-11 20:57:48 +0100

    Update DepAction.java

--- src/cn/itcast/invoice/auth/dep/web/DepAction.java
+++ src/cn/itcast/invoice/auth/dep/web/DepAction.java
@@ -7,21 +7,39 @@ import cn.itcast.invoice.auth.dep.vo.DepModel;
 import cn.itcast.invoice.auth.dep.vo.DepQueryModel;
 import cn.itcast.invoice.util.base.BaseAction;
 
+
+/**
+ * this interface extends BaseAction
+ *
+ */
+
 public class DepAction extends BaseAction{
 
+	/**
+	 * public field
+	 */
 	public DepModel dm = new DepModel();
+	/**
+	 * public field
+	 */
 	public DepQueryModel dqm = new DepQueryModel();
 	
 	private DepEbi depEbi;
+	/**
+	 * public field
+	 */
 	public void setDepEbi(DepEbi depEbi) {
 		this.depEbi = depEbi;
 	}
 	
-	//跳转到列表页面
+	//Ã¨Â·Â³Ã¨Â½Â¬Ã¥Ë†Â°Ã¥Ë†â€”Ã¨Â¡Â¨Ã©Â¡ÂµÃ©ï¿½Â¢
+	/**
+	 * public field
+	 */
 	public String list(){
-		//计算最大页码值 : 记录总条目数与每页显示数计算而来
+		//Ã¨Â®Â¡Ã§Â®â€”Ã¦Å“â‚¬Ã¥Â¤Â§Ã©Â¡ÂµÃ§Â ï¿½Ã¥â‚¬Â¼ : Ã¨Â®Â°Ã¥Â½â€¢Ã¦â‚¬Â»Ã¦ï¿½Â¡Ã§â€ºÂ®Ã¦â€¢Â°Ã¤Â¸Å½Ã¦Â¯ï¿½Ã©Â¡ÂµÃ¦ËœÂ¾Ã§Â¤ÂºÃ¦â€¢Â°Ã¨Â®Â¡Ã§Â®â€”Ã¨â‚¬Å’Ã¦ï¿½Â¥
 		//dataTotal = depEbi.getCount(dqm);
-		//最大页码值
+		//Ã¦Å“â‚¬Ã¥Â¤Â§Ã©Â¡ÂµÃ§Â ï¿½Ã¥â‚¬Â¼
 		/*
 		37  10 	4
 		(37+10 -1) /10 	4
@@ -34,50 +52,59 @@ public class DepAction extends BaseAction{
 		*/
 		//maxPageNum = (dataTotal + pageCount -1) / pageCount; 
 		setDataTotal(depEbi.getCount(dqm));
-		//获取所有的部门数据
+		//Ã¨Å½Â·Ã¥ï¿½â€“Ã¦â€°â‚¬Ã¦Å“â€°Ã§Å¡â€žÃ©Æ’Â¨Ã©â€”Â¨Ã¦â€¢Â°Ã¦ï¿½Â®
 		List<DepModel> depList = depEbi.getAll(dqm,pageNum,pageCount);
-		//将该数据放入指定范围
+		//Ã¥Â°â€ Ã¨Â¯Â¥Ã¦â€¢Â°Ã¦ï¿½Â®Ã¦â€�Â¾Ã¥â€¦Â¥Ã¦Å’â€¡Ã¥Â®Å¡Ã¨Å’Æ’Ã¥â€ºÂ´
 		//ActionContext.getContext().put("depList", depList);
 		put("depList",depList);
-		//跳转页面
-		//页面中从指定范围内获取数据展示
+		//Ã¨Â·Â³Ã¨Â½Â¬Ã©Â¡ÂµÃ©ï¿½Â¢
+		//Ã©Â¡ÂµÃ©ï¿½Â¢Ã¤Â¸Â­Ã¤Â»Å½Ã¦Å’â€¡Ã¥Â®Å¡Ã¨Å’Æ’Ã¥â€ºÂ´Ã¥â€ â€¦Ã¨Å½Â·Ã¥ï¿½â€“Ã¦â€¢Â°Ã¦ï¿½Â®Ã¥Â±â€¢Ã§Â¤Âº
 		return LIST;
 	}
 	
 	/*
-	//按条件查询
+	//Ã¦Å’â€°Ã¦ï¿½Â¡Ã¤Â»Â¶Ã¦Å¸Â¥Ã¨Â¯Â¢
 	public String queryList(){
-		//根据查询条件进行查询获得最终显示的数据
+		//Ã¦Â Â¹Ã¦ï¿½Â®Ã¦Å¸Â¥Ã¨Â¯Â¢Ã¦ï¿½Â¡Ã¤Â»Â¶Ã¨Â¿â€ºÃ¨Â¡Å’Ã¦Å¸Â¥Ã¨Â¯Â¢Ã¨Å½Â·Ã¥Â¾â€”Ã¦Å“â‚¬Ã§Â»Ë†Ã¦ËœÂ¾Ã§Â¤ÂºÃ§Å¡â€žÃ¦â€¢Â°Ã¦ï¿½Â®
 		List<DepModel> depList = depEbi.getAll(dqm);
-		//放入指定范围
+		//Ã¦â€�Â¾Ã¥â€¦Â¥Ã¦Å’â€¡Ã¥Â®Å¡Ã¨Å’Æ’Ã¥â€ºÂ´
 		ActionContext.getContext().put("depList",depList);
-		//跳转页面
+		//Ã¨Â·Â³Ã¨Â½Â¬Ã©Â¡ÂµÃ©ï¿½Â¢
 		return "list";
 	}
 	*/
 	
-	//新建部门
+	//Ã¦â€“Â°Ã¥Â»ÂºÃ©Æ’Â¨Ã©â€”Â¨
+	/**
+	 * public field
+	 */
 	public String save(){
-		//通过页面是否传递有具体的uuid值来区分究竟是添加还是修改
-		if(dm.getUuid()== null){	//没有传递uuid值，添加
+		//Ã©â‚¬Å¡Ã¨Â¿â€¡Ã©Â¡ÂµÃ©ï¿½Â¢Ã¦ËœÂ¯Ã¥ï¿½Â¦Ã¤Â¼Â Ã©â‚¬â€™Ã¦Å“â€°Ã¥â€¦Â·Ã¤Â½â€œÃ§Å¡â€žuuidÃ¥â‚¬Â¼Ã¦ï¿½Â¥Ã¥Å’ÂºÃ¥Ë†â€ Ã§Â©Â¶Ã§Â«Å¸Ã¦ËœÂ¯Ã¦Â·Â»Ã¥Å Â Ã¨Â¿ËœÃ¦ËœÂ¯Ã¤Â¿Â®Ã¦â€�Â¹
+		if(dm.getUuid()== null){	//Ã¦Â²Â¡Ã¦Å“â€°Ã¤Â¼Â Ã©â‚¬â€™uuidÃ¥â‚¬Â¼Ã¯Â¼Å’Ã¦Â·Â»Ã¥Å Â 
 			depEbi.save(dm);
-		}else{						//传递了uuid值，修改
+		}else{						//Ã¤Â¼Â Ã©â‚¬â€™Ã¤Âºâ€ uuidÃ¥â‚¬Â¼Ã¯Â¼Å’Ã¤Â¿Â®Ã¦â€�Â¹
 			depEbi.update(dm);
 		}
 		return TO_LIST;
 	}
 	
-	//跳转到修改页面
+	//Ã¨Â·Â³Ã¨Â½Â¬Ã¥Ë†Â°Ã¤Â¿Â®Ã¦â€�Â¹Ã©Â¡ÂµÃ©ï¿½Â¢
+	/**
+	 * public field
+	 */
 	public String input(){
-		//如果传递有uuid，执行查询
+		//Ã¥Â¦â€šÃ¦Å¾Å“Ã¤Â¼Â Ã©â‚¬â€™Ã¦Å“â€°uuidÃ¯Â¼Å’Ã¦â€°Â§Ã¨Â¡Å’Ã¦Å¸Â¥Ã¨Â¯Â¢
 		if(dm.getUuid()!=null){
-			//根据传递的uuid获取对应的数据
+			//Ã¦Â Â¹Ã¦ï¿½Â®Ã¤Â¼Â Ã©â‚¬â€™Ã§Å¡â€žuuidÃ¨Å½Â·Ã¥ï¿½â€“Ã¥Â¯Â¹Ã¥Âºâ€�Ã§Å¡â€žÃ¦â€¢Â°Ã¦ï¿½Â®
 			dm = depEbi.get(dm.getUuid());
 		}
 		return INPUT;
 	}
 	
-	//删除部门信息
+	//Ã¥Ë†Â Ã©â„¢Â¤Ã©Æ’Â¨Ã©â€”Â¨Ã¤Â¿Â¡Ã¦ï¿½Â¯
+	/**
+	 * public field
+	 */
 	public String delete(){
 		depEbi.delete(dm);
 		return TO_LIST;
@@ -100,11 +127,11 @@ public class DepAction extends ActionSupport{
 	public Integer pageCount = 2;
 	public Integer maxPageNum ;
 	public Integer dataTotal;
-	//跳转到列表页面
+	//Ã¨Â·Â³Ã¨Â½Â¬Ã¥Ë†Â°Ã¥Ë†â€”Ã¨Â¡Â¨Ã©Â¡ÂµÃ©ï¿½Â¢
 	public String list(){
-		//计算最大页码值 : 记录总条目数与每页显示数计算而来
+		//Ã¨Â®Â¡Ã§Â®â€”Ã¦Å“â‚¬Ã¥Â¤Â§Ã©Â¡ÂµÃ§Â ï¿½Ã¥â‚¬Â¼ : Ã¨Â®Â°Ã¥Â½â€¢Ã¦â‚¬Â»Ã¦ï¿½Â¡Ã§â€ºÂ®Ã¦â€¢Â°Ã¤Â¸Å½Ã¦Â¯ï¿½Ã©Â¡ÂµÃ¦ËœÂ¾Ã§Â¤ÂºÃ¦â€¢Â°Ã¨Â®Â¡Ã§Â®â€”Ã¨â‚¬Å’Ã¦ï¿½Â¥
 		dataTotal = depEbi.getCount(dqm);
-		//最大页码值
+		//Ã¦Å“â‚¬Ã¥Â¤Â§Ã©Â¡ÂµÃ§Â ï¿½Ã¥â‚¬Â¼
 		37  10 	4
 		(37+10 -1) /10 	4
 		39	10	4
@@ -114,47 +141,47 @@ public class DepAction extends ActionSupport{
 		41  10	5
 		(41+10-1) /10  5
 		maxPageNum = (dataTotal + pageCount -1) / pageCount; 
-		//获取所有的部门数据
+		//Ã¨Å½Â·Ã¥ï¿½â€“Ã¦â€°â‚¬Ã¦Å“â€°Ã§Å¡â€žÃ©Æ’Â¨Ã©â€”Â¨Ã¦â€¢Â°Ã¦ï¿½Â®
 		List<DepModel> depList = depEbi.getAll(dqm,pageNum,pageCount);
-		//将该数据放入指定范围
+		//Ã¥Â°â€ Ã¨Â¯Â¥Ã¦â€¢Â°Ã¦ï¿½Â®Ã¦â€�Â¾Ã¥â€¦Â¥Ã¦Å’â€¡Ã¥Â®Å¡Ã¨Å’Æ’Ã¥â€ºÂ´
 		ActionContext.getContext().put("depList", depList);
-		//跳转页面
-		//页面中从指定范围内获取数据展示
+		//Ã¨Â·Â³Ã¨Â½Â¬Ã©Â¡ÂµÃ©ï¿½Â¢
+		//Ã©Â¡ÂµÃ©ï¿½Â¢Ã¤Â¸Â­Ã¤Â»Å½Ã¦Å’â€¡Ã¥Â®Å¡Ã¨Å’Æ’Ã¥â€ºÂ´Ã¥â€ â€¦Ã¨Å½Â·Ã¥ï¿½â€“Ã¦â€¢Â°Ã¦ï¿½Â®Ã¥Â±â€¢Ã§Â¤Âº
 		return "list";
 	}
 	
-	//按条件查询
+	//Ã¦Å’â€°Ã¦ï¿½Â¡Ã¤Â»Â¶Ã¦Å¸Â¥Ã¨Â¯Â¢
 	public String queryList(){
-		//根据查询条件进行查询获得最终显示的数据
+		//Ã¦Â Â¹Ã¦ï¿½Â®Ã¦Å¸Â¥Ã¨Â¯Â¢Ã¦ï¿½Â¡Ã¤Â»Â¶Ã¨Â¿â€ºÃ¨Â¡Å’Ã¦Å¸Â¥Ã¨Â¯Â¢Ã¨Å½Â·Ã¥Â¾â€”Ã¦Å“â‚¬Ã§Â»Ë†Ã¦ËœÂ¾Ã§Â¤ÂºÃ§Å¡â€žÃ¦â€¢Â°Ã¦ï¿½Â®
 		List<DepModel> depList = depEbi.getAll(dqm);
-		//放入指定范围
+		//Ã¦â€�Â¾Ã¥â€¦Â¥Ã¦Å’â€¡Ã¥Â®Å¡Ã¨Å’Æ’Ã¥â€ºÂ´
 		ActionContext.getContext().put("depList",depList);
-		//跳转页面
+		//Ã¨Â·Â³Ã¨Â½Â¬Ã©Â¡ÂµÃ©ï¿½Â¢
 		return "list";
 	}
 	
-	//新建部门
+	//Ã¦â€“Â°Ã¥Â»ÂºÃ©Æ’Â¨Ã©â€”Â¨
 	public String save(){
-		//通过页面是否传递有具体的uuid值来区分究竟是添加还是修改
-		if(dm.getUuid()== null){	//没有传递uuid值，添加
+		//Ã©â‚¬Å¡Ã¨Â¿â€¡Ã©Â¡ÂµÃ©ï¿½Â¢Ã¦ËœÂ¯Ã¥ï¿½Â¦Ã¤Â¼Â Ã©â‚¬â€™Ã¦Å“â€°Ã¥â€¦Â·Ã¤Â½â€œÃ§Å¡â€žuuidÃ¥â‚¬Â¼Ã¦ï¿½Â¥Ã¥Å’ÂºÃ¥Ë†â€ Ã§Â©Â¶Ã§Â«Å¸Ã¦ËœÂ¯Ã¦Â·Â»Ã¥Å Â Ã¨Â¿ËœÃ¦ËœÂ¯Ã¤Â¿Â®Ã¦â€�Â¹
+		if(dm.getUuid()== null){	//Ã¦Â²Â¡Ã¦Å“â€°Ã¤Â¼Â Ã©â‚¬â€™uuidÃ¥â‚¬Â¼Ã¯Â¼Å’Ã¦Â·Â»Ã¥Å Â 
 			depEbi.save(dm);
-		}else{						//传递了uuid值，修改
+		}else{						//Ã¤Â¼Â Ã©â‚¬â€™Ã¤Âºâ€ uuidÃ¥â‚¬Â¼Ã¯Â¼Å’Ã¤Â¿Â®Ã¦â€�Â¹
 			depEbi.update(dm);
 		}
 		return "toList";
 	}
 	
-	//跳转到修改页面
+	//Ã¨Â·Â³Ã¨Â½Â¬Ã¥Ë†Â°Ã¤Â¿Â®Ã¦â€�Â¹Ã©Â¡ÂµÃ©ï¿½Â¢
 	public String input(){
-		//如果传递有uuid，执行查询
+		//Ã¥Â¦â€šÃ¦Å¾Å“Ã¤Â¼Â Ã©â‚¬â€™Ã¦Å“â€°uuidÃ¯Â¼Å’Ã¦â€°Â§Ã¨Â¡Å’Ã¦Å¸Â¥Ã¨Â¯Â¢
 		if(dm.getUuid()!=null){
-			//根据传递的uuid获取对应的数据
+			//Ã¦Â Â¹Ã¦ï¿½Â®Ã¤Â¼Â Ã©â‚¬â€™Ã§Å¡â€žuuidÃ¨Å½Â·Ã¥ï¿½â€“Ã¥Â¯Â¹Ã¥Âºâ€�Ã§Å¡â€žÃ¦â€¢Â°Ã¦ï¿½Â®
 			dm = depEbi.get(dm.getUuid());
 		}
 		return "input";
 	}
 	
-	//删除部门信息
+	//Ã¥Ë†Â Ã©â„¢Â¤Ã©Æ’Â¨Ã©â€”Â¨Ã¤Â¿Â¡Ã¦ï¿½Â¯
 	public String delete(){
 		depEbi.delete(dm);
 		return "toList";
