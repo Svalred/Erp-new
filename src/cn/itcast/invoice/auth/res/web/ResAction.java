828d558bd3d7ea46e45ab57570a6eec55bd56069
Svalred <34680485+Svalred@users.noreply.github.com>
2018-02-11 21:03:09 +0100

    Update ResAction.java

--- src/cn/itcast/invoice/auth/res/web/ResAction.java
+++ src/cn/itcast/invoice/auth/res/web/ResAction.java
@@ -8,23 +8,54 @@ import cn.itcast.invoice.auth.res.vo.ResQueryModel;
 import cn.itcast.invoice.auth.role.business.ebi.RoleEbi;
 import cn.itcast.invoice.auth.role.vo.RoleModel;
 import cn.itcast.invoice.util.base.BaseAction;
-
+/**
+ * this class extends BaseAction
+ *
+ */
 public class ResAction extends BaseAction{
+	/**
+	 * this public element is a public element
+	 *
+	 */
 	public ResModel rm = new ResModel();
+	/**
+	 * this public element is a public element
+	 *
+	 */
 	public ResQueryModel rqm = new ResQueryModel();
-
+	/**
+	 * this public element is a public element
+	 *
+	 */
+	
 	private ResEbi resEbi;
+	/**
+	 * this public element is a public element
+	 *
+	 */
 	private RoleEbi roleEbi;
 	
+	/**
+	 * this public element is a public element
+	 *
+	 */
 	public void setRoleEbi(RoleEbi roleEbi) {
 		this.roleEbi = roleEbi;
 	}
 
+	/**
+	 * this public element is a public element
+	 *
+	 */
 	public void setResEbi(ResEbi resEbi) {
 		this.resEbi = resEbi;
 	}
 
-	//跳转到列表页面
+	//Ã¨Â·Â³Ã¨Â½Â¬Ã¥Ë†Â°Ã¥Ë†â€”Ã¨Â¡Â¨Ã©Â¡ÂµÃ©ï¿½Â¢
+	/**
+	 * this public element is a public element
+	 *
+	 */
 	public String list(){
 		setDataTotal(resEbi.getCount(rqm));
 		List<ResModel> resList = resEbi.getAll(rqm,pageNum,pageCount);
@@ -32,8 +63,16 @@ public class ResAction extends BaseAction{
 		return LIST;
 	}
 	
+	/**
+	 * this public element is a public element
+	 *
+	 */
 	public Long[] roleUuids;
-	//保存/修改
+	//Ã¤Â¿ï¿½Ã¥Â­Ëœ/Ã¤Â¿Â®Ã¦â€�Â¹
+	/**
+	 * this public element is a public element
+	 *
+	 */
 	public String save(){
 		if(rm.getUuid()== null){
 			resEbi.save(rm,roleUuids);
@@ -43,13 +82,17 @@ public class ResAction extends BaseAction{
 		return TO_LIST;
 	}
 
-	//跳转到添加/修改页面
+	//Ã¨Â·Â³Ã¨Â½Â¬Ã¥Ë†Â°Ã¦Â·Â»Ã¥Å Â /Ã¤Â¿Â®Ã¦â€�Â¹Ã©Â¡ÂµÃ©ï¿½Â¢
+	/**
+	 * this public element is a public element
+	 *
+	 */
 	public String input(){
 		List<RoleModel> roleList = roleEbi.getAll();
 		put("roleList",roleList);
 		if(rm.getUuid()!=null){
 			rm = resEbi.get(rm.getUuid());
-			//将集合转换为数组
+			//Ã¥Â°â€ Ã©â€ºâ€ Ã¥ï¿½Ë†Ã¨Â½Â¬Ã¦ï¿½Â¢Ã¤Â¸ÂºÃ¦â€¢Â°Ã§Â»â€ž
 			roleUuids = new Long[rm.getRoles().size()];
 			int i = 0;
 			for(RoleModel temp : rm.getRoles()){
@@ -59,7 +102,11 @@ public class ResAction extends BaseAction{
 		return INPUT;
 	}
 
-	//删除
+	//Ã¥Ë†Â Ã©â„¢Â¤
+	/**
+	 * this public element is a public element
+	 *
+	 */
 	public String delete(){
 		resEbi.delete(rm);
 		return TO_LIST;
