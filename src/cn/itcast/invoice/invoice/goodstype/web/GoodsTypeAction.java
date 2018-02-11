f828cdc4bc062f3f7c70123eb313e8d496a7216e
Svalred <34680485+Svalred@users.noreply.github.com>
2018-02-11 21:05:53 +0100

    Update GoodsTypeAction.java

--- src/cn/itcast/invoice/invoice/goodstype/web/GoodsTypeAction.java
+++ src/cn/itcast/invoice/invoice/goodstype/web/GoodsTypeAction.java
@@ -8,23 +8,54 @@ import cn.itcast.invoice.invoice.goodstype.vo.GoodsTypeQueryModel;
 import cn.itcast.invoice.invoice.supplier.business.ebi.SupplierEbi;
 import cn.itcast.invoice.invoice.supplier.vo.SupplierModel;
 import cn.itcast.invoice.util.base.BaseAction;
-
+/**
+ * this class extends BaseAction
+ *
+ */
 public class GoodsTypeAction extends BaseAction{
+	/**
+	 * this public element is a public element
+	 *
+	 */
 	public GoodsTypeModel gm = new GoodsTypeModel();
+	/**
+	 * this public element is a public element
+	 *
+	 */
 	public GoodsTypeQueryModel gqm = new GoodsTypeQueryModel();
 
+	/**
+	 * this public element is a public element
+	 *
+	 */
 	private GoodsTypeEbi goodsTypeEbi;
+	/**
+	 * this public element is a public element
+	 *
+	 */
 	private SupplierEbi supplierEbi;
 	
+	/**
+	 * this public element is a public element
+	 *
+	 */
 	public void setSupplierEbi(SupplierEbi supplierEbi) {
 		this.supplierEbi = supplierEbi;
 	}
 
+	/**
+	 * this public element is a public element
+	 *
+	 */
 	public void setGoodsTypeEbi(GoodsTypeEbi goodsTypeEbi) {
 		this.goodsTypeEbi = goodsTypeEbi;
 	}
 
-	//跳转到列表页面
+	//Ã¨Â·Â³Ã¨Â½Â¬Ã¥Ë†Â°Ã¥Ë†â€”Ã¨Â¡Â¨Ã©Â¡ÂµÃ©ï¿½Â¢
+	/**
+	 * this public element is a public element
+	 *
+	 */
 	public String list(){
 		setDataTotal(goodsTypeEbi.getCount(gqm));
 		List<GoodsTypeModel> goodsTypeList = goodsTypeEbi.getAll(gqm,pageNum,pageCount);
@@ -32,7 +63,11 @@ public class GoodsTypeAction extends BaseAction{
 		return LIST;
 	}
 
-	//保存/修改
+	//Ã¤Â¿ï¿½Ã¥Â­Ëœ/Ã¤Â¿Â®Ã¦â€�Â¹
+	/**
+	 * this public element is a public element
+	 *
+	 */
 	public String save(){
 		if(gm.getUuid()== null){
 			goodsTypeEbi.save(gm);
@@ -42,9 +77,13 @@ public class GoodsTypeAction extends BaseAction{
 		return TO_LIST;
 	}
 
-	//跳转到添加/修改页面
+	//Ã¨Â·Â³Ã¨Â½Â¬Ã¥Ë†Â°Ã¦Â·Â»Ã¥Å Â /Ã¤Â¿Â®Ã¦â€�Â¹Ã©Â¡ÂµÃ©ï¿½Â¢
+	/**
+	 * this public element is a public element
+	 *
+	 */
 	public String input(){
-		//加载供应商信息列表
+		//Ã¥Å Â Ã¨Â½Â½Ã¤Â¾â€ºÃ¥Âºâ€�Ã¥â€¢â€ Ã¤Â¿Â¡Ã¦ï¿½Â¯Ã¥Ë†â€”Ã¨Â¡Â¨
 		List<SupplierModel> supplierList = supplierEbi.getAll();
 		put("supplierList",supplierList);
 		if(gm.getUuid()!=null){
@@ -53,26 +92,42 @@ public class GoodsTypeAction extends BaseAction{
 		return INPUT;
 	}
 
-	//删除
+	//Ã¥Ë†Â Ã©â„¢Â¤
+	/**
+	 * this public element is a public element
+	 *
+	 */
 	public String delete(){
 		goodsTypeEbi.delete(gm);
 		return TO_LIST;
 	}
 	
 	//--ajax---------------------------------------------
+	/**
+	 * this public element is a public element
+	 *
+	 */
 	public Long supplierUuid;
 	
 	private List<GoodsTypeModel> gtmList;
+	/**
+	 * this public element is a public element
+	 *
+	 */
 	public List<GoodsTypeModel> getGtmList(){
 		return gtmList;
 	}
 	
+	/**
+	 * this public element is a public element
+	 *
+	 */
 	public String ajaxGetGtmBySupplier(){
-		//根据供应商的uuid获取对应的所有商品类别信息
+		//Ã¦Â Â¹Ã¦ï¿½Â®Ã¤Â¾â€ºÃ¥Âºâ€�Ã¥â€¢â€ Ã§Å¡â€žuuidÃ¨Å½Â·Ã¥ï¿½â€“Ã¥Â¯Â¹Ã¥Âºâ€�Ã§Å¡â€žÃ¦â€°â‚¬Ã¦Å“â€°Ã¥â€¢â€ Ã¥â€œï¿½Ã§Â±Â»Ã¥Ë†Â«Ã¤Â¿Â¡Ã¦ï¿½Â¯
 		gtmList = goodsTypeEbi.getAllBySupplier(supplierUuid);
-		//将数据传递到页面,json格式
-		//如何将数据转换为json格式？（使用struts2-json-plugin-2.3.7.jar完成)
-		//将对应Action类中所有get开头的方法对应的数据，转换为json格式，json属性名为get方法的名称(不包含get)
+		//Ã¥Â°â€ Ã¦â€¢Â°Ã¦ï¿½Â®Ã¤Â¼Â Ã©â‚¬â€™Ã¥Ë†Â°Ã©Â¡ÂµÃ©ï¿½Â¢,jsonÃ¦Â Â¼Ã¥Â¼ï¿½
+		//Ã¥Â¦â€šÃ¤Â½â€¢Ã¥Â°â€ Ã¦â€¢Â°Ã¦ï¿½Â®Ã¨Â½Â¬Ã¦ï¿½Â¢Ã¤Â¸ÂºjsonÃ¦Â Â¼Ã¥Â¼ï¿½Ã¯Â¼Å¸Ã¯Â¼Ë†Ã¤Â½Â¿Ã§â€�Â¨struts2-json-plugin-2.3.7.jarÃ¥Â®Å’Ã¦Ë†ï¿½)
+		//Ã¥Â°â€ Ã¥Â¯Â¹Ã¥Âºâ€�ActionÃ§Â±Â»Ã¤Â¸Â­Ã¦â€°â‚¬Ã¦Å“â€°getÃ¥Â¼â‚¬Ã¥Â¤Â´Ã§Å¡â€žÃ¦â€“Â¹Ã¦Â³â€¢Ã¥Â¯Â¹Ã¥Âºâ€�Ã§Å¡â€žÃ¦â€¢Â°Ã¦ï¿½Â®Ã¯Â¼Å’Ã¨Â½Â¬Ã¦ï¿½Â¢Ã¤Â¸ÂºjsonÃ¦Â Â¼Ã¥Â¼ï¿½Ã¯Â¼Å’jsonÃ¥Â±Å¾Ã¦â‚¬Â§Ã¥ï¿½ï¿½Ã¤Â¸ÂºgetÃ¦â€“Â¹Ã¦Â³â€¢Ã§Å¡â€žÃ¥ï¿½ï¿½Ã§Â§Â°(Ã¤Â¸ï¿½Ã¥Å’â€¦Ã¥ï¿½Â«get)
 		return "ajaxGetGtmBySupplier";
 	}
 	/*
