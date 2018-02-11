af987e289982bad7e0a9280fa0593e0cbe99160e
Svalred <34680485+Svalred@users.noreply.github.com>
2018-02-11 21:09:52 +0100

    Update StoreDetailAction.java

--- src/cn/itcast/invoice/invoice/storedetail/web/StoreDetailAction.java
+++ src/cn/itcast/invoice/invoice/storedetail/web/StoreDetailAction.java
@@ -6,17 +6,36 @@ import cn.itcast.invoice.invoice.storedetail.business.ebi.StoreDetailEbi;
 import cn.itcast.invoice.invoice.storedetail.vo.StoreDetailModel;
 import cn.itcast.invoice.invoice.storedetail.vo.StoreDetailQueryModel;
 import cn.itcast.invoice.util.base.BaseAction;
-
+/**
+ * this class extends BaseAction
+ *
+ */
 public class StoreDetailAction extends BaseAction{
+	/**
+	 * this public element is a public element
+	 *
+	 */
 	public StoreDetailModel sm = new StoreDetailModel();
+	/**
+	 * this public element is a public element
+	 *
+	 */
 	public StoreDetailQueryModel sqm = new StoreDetailQueryModel();
 
 	private StoreDetailEbi storeDetailEbi;
+	/**
+	 * this public element is a public element
+	 *
+	 */
 	public void setStoreDetailEbi(StoreDetailEbi storeDetailEbi) {
 		this.storeDetailEbi = storeDetailEbi;
 	}
 
-	//跳转到列表页面
+	//Ã¨Â·Â³Ã¨Â½Â¬Ã¥Ë†Â°Ã¥Ë†â€”Ã¨Â¡Â¨Ã©Â¡ÂµÃ©ï¿½Â¢
+	/**
+	 * this public element is a public element
+	 *
+	 */
 	public String list(){
 		setDataTotal(storeDetailEbi.getCount(sqm));
 		List<StoreDetailModel> storeDetailList = storeDetailEbi.getAll(sqm,pageNum,pageCount);
@@ -24,7 +43,11 @@ public class StoreDetailAction extends BaseAction{
 		return LIST;
 	}
 
-	//保存/修改
+	//Ã¤Â¿ï¿½Ã¥Â­Ëœ/Ã¤Â¿Â®Ã¦â€�Â¹
+	/**
+	 * this public element is a public element
+	 *
+	 */
 	public String save(){
 		if(sm.getUuid()== null){
 			storeDetailEbi.save(sm);
@@ -34,7 +57,11 @@ public class StoreDetailAction extends BaseAction{
 		return TO_LIST;
 	}
 
-	//跳转到添加/修改页面
+	//Ã¨Â·Â³Ã¨Â½Â¬Ã¥Ë†Â°Ã¦Â·Â»Ã¥Å Â /Ã¤Â¿Â®Ã¦â€�Â¹Ã©Â¡ÂµÃ©ï¿½Â¢
+	/**
+	 * this public element is a public element
+	 *
+	 */
 	public String input(){
 		if(sm.getUuid()!=null){
 			sm = storeDetailEbi.get(sm.getUuid());
@@ -42,7 +69,11 @@ public class StoreDetailAction extends BaseAction{
 		return INPUT;
 	}
 
-	//删除
+	//Ã¥Ë†Â Ã©â„¢Â¤
+	/**
+	 * this public element is a public element
+	 *
+	 */
 	public String delete(){
 		storeDetailEbi.delete(sm);
 		return TO_LIST;
