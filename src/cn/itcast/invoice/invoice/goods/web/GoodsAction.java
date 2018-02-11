8ffbf560132c44f1add95a4ac5055822c6c38573
Svalred <34680485+Svalred@users.noreply.github.com>
2018-02-11 21:05:18 +0100

    Update GoodsAction.java

--- src/cn/itcast/invoice/invoice/goods/web/GoodsAction.java
+++ src/cn/itcast/invoice/invoice/goods/web/GoodsAction.java
@@ -10,30 +10,57 @@ import cn.itcast.invoice.invoice.goodstype.vo.GoodsTypeModel;
 import cn.itcast.invoice.invoice.supplier.business.ebi.SupplierEbi;
 import cn.itcast.invoice.invoice.supplier.vo.SupplierModel;
 import cn.itcast.invoice.util.base.BaseAction;
-
+/**
+ * this class extends BaseAction
+ *
+ */
 public class GoodsAction extends BaseAction{
+	/**
+	 * this public element is a public element
+	 *
+	 */
 	public GoodsModel gm = new GoodsModel();
+	/**
+	 * this public element is a public element
+	 *
+	 */
 	public GoodsQueryModel gqm = new GoodsQueryModel();
 
 	private GoodsEbi goodsEbi;
 	private SupplierEbi supplierEbi;
 	private GoodsTypeEbi goodsTypeEbi;
 	
+	/**
+	 * this public element is a public element
+	 *
+	 */
 	public void setGoodsTypeEbi(GoodsTypeEbi goodsTypeEbi) {
 		this.goodsTypeEbi = goodsTypeEbi;
 	}
 
+	/**
+	 * this public element is a public element
+	 *
+	 */
 	public void setSupplierEbi(SupplierEbi supplierEbi) {
 		this.supplierEbi = supplierEbi;
 	}
 
+	/**
+	 * this public element is a public element
+	 *
+	 */
 	public void setGoodsEbi(GoodsEbi goodsEbi) {
 		this.goodsEbi = goodsEbi;
 	}
 
-	//跳转到列表页面
+	//Ã¨Â·Â³Ã¨Â½Â¬Ã¥Ë†Â°Ã¥Ë†â€”Ã¨Â¡Â¨Ã©Â¡ÂµÃ©ï¿½Â¢
+	/**
+	 * this public element is a public element
+	 *
+	 */
 	public String list(){
-		//加载供应商的全部数据
+		//Ã¥Å Â Ã¨Â½Â½Ã¤Â¾â€ºÃ¥Âºâ€�Ã¥â€¢â€ Ã§Å¡â€žÃ¥â€¦Â¨Ã©Æ’Â¨Ã¦â€¢Â°Ã¦ï¿½Â®
 		List<SupplierModel> supplierList = supplierEbi.getAll();
 		put("supplierList",supplierList);
 		setDataTotal(goodsEbi.getCount(gqm));
@@ -42,7 +69,11 @@ public class GoodsAction extends BaseAction{
 		return LIST;
 	}
 
-	//保存/修改
+	//Ã¤Â¿ï¿½Ã¥Â­Ëœ/Ã¤Â¿Â®Ã¦â€�Â¹
+	/**
+	 * this public element is a public element
+	 *
+	 */
 	public String save(){
 		if(gm.getUuid()== null){
 			goodsEbi.save(gm);
@@ -52,12 +83,16 @@ public class GoodsAction extends BaseAction{
 		return TO_LIST;
 	}
 
-	//跳转到添加/修改页面
+	//Ã¨Â·Â³Ã¨Â½Â¬Ã¥Ë†Â°Ã¦Â·Â»Ã¥Å Â /Ã¤Â¿Â®Ã¦â€�Â¹Ã©Â¡ÂµÃ©ï¿½Â¢
+	/**
+	 * this public element is a public element
+	 *
+	 */
 	public String input(){
-		//加载所有的具有商品类别的供应商信息
+		//Ã¥Å Â Ã¨Â½Â½Ã¦â€°â‚¬Ã¦Å“â€°Ã§Å¡â€žÃ¥â€¦Â·Ã¦Å“â€°Ã¥â€¢â€ Ã¥â€œï¿½Ã§Â±Â»Ã¥Ë†Â«Ã§Å¡â€žÃ¤Â¾â€ºÃ¥Âºâ€�Ã¥â€¢â€ Ã¤Â¿Â¡Ã¦ï¿½Â¯
 		List<SupplierModel> supplierList = supplierEbi.getAllUnion();
 		put("supplierList",supplierList);
-		//加载第一个供应商对应的商品类别信息
+		//Ã¥Å Â Ã¨Â½Â½Ã§Â¬Â¬Ã¤Â¸â‚¬Ã¤Â¸ÂªÃ¤Â¾â€ºÃ¥Âºâ€�Ã¥â€¢â€ Ã¥Â¯Â¹Ã¥Âºâ€�Ã§Å¡â€žÃ¥â€¢â€ Ã¥â€œï¿½Ã§Â±Â»Ã¥Ë†Â«Ã¤Â¿Â¡Ã¦ï¿½Â¯
 		List<GoodsTypeModel> gtmList = goodsTypeEbi.getAllBySupplier(supplierList.get(0).getUuid());
 		put("gtmList",gtmList);
 		if(gm.getUuid()!=null){
@@ -66,7 +101,11 @@ public class GoodsAction extends BaseAction{
 		return INPUT;
 	}
 
-	//删除
+	//Ã¥Ë†Â Ã©â„¢Â¤
+	/**
+	 * this public element is a public element
+	 *
+	 */
 	public String delete(){
 		goodsEbi.delete(gm);
 		return TO_LIST;
