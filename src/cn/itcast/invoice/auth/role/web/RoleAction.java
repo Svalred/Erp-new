62555ee4e09334d430a864c01a167fba242dc7f5
Svalred <34680485+Svalred@users.noreply.github.com>
2018-02-11 21:04:00 +0100

    Update RoleAction.java

--- src/cn/itcast/invoice/auth/role/web/RoleAction.java
+++ src/cn/itcast/invoice/auth/role/web/RoleAction.java
@@ -10,39 +10,79 @@ import cn.itcast.invoice.auth.role.business.ebi.RoleEbi;
 import cn.itcast.invoice.auth.role.vo.RoleModel;
 import cn.itcast.invoice.auth.role.vo.RoleQueryModel;
 import cn.itcast.invoice.util.base.BaseAction;
-
+/**
+ * this class extends BaseAction
+ *
+ */
 public class RoleAction extends BaseAction{
+	
+	/**
+	 * this public element is a public element
+	 *
+	 */
 	public RoleModel rm = new RoleModel();
+	/**
+	 * this public element is a public element
+	 *
+	 */
 	public RoleQueryModel rqm = new RoleQueryModel();
 
 	private RoleEbi roleEbi;
 	private ResEbi resEbi;
 	private MenuEbi menuEbi;
 	
+	/**
+	 * this public element is a public element
+	 *
+	 */
 	public void setMenuEbi(MenuEbi menuEbi) {
 		this.menuEbi = menuEbi;
 	}
 
+	/**
+	 * this public element is a public element
+	 *
+	 */
 	public void setResEbi(ResEbi resEbi) {
 		this.resEbi = resEbi;
 	}
 
+	/**
+	 * this public element is a public element
+	 *
+	 */
 	public void setRoleEbi(RoleEbi roleEbi) {
 		this.roleEbi = roleEbi;
 	}
 
-	//跳转到列表页面
+	//Ã¨Â·Â³Ã¨Â½Â¬Ã¥Ë†Â°Ã¥Ë†â€”Ã¨Â¡Â¨Ã©Â¡ÂµÃ©ï¿½Â¢
+	/**
+	 * this public element is a public element
+	 *
+	 */
 	public String list(){
 		setDataTotal(roleEbi.getCount(rqm));
 		List<RoleModel> roleList = roleEbi.getAll(rqm,pageNum,pageCount);
 		put("roleList",roleList);
 		return LIST;
 	}
-	//资源的uuid数组
+	//Ã¨Âµâ€žÃ¦Âºï¿½Ã§Å¡â€žuuidÃ¦â€¢Â°Ã§Â»â€ž
+	/**
+	 * this public element is a public element
+	 *
+	 */
 	public Long[] resUuids;
-	//菜单的uuid数组
+	//Ã¨ï¿½Å“Ã¥ï¿½â€¢Ã§Å¡â€žuuidÃ¦â€¢Â°Ã§Â»â€ž
+	/**
+	 * this public element is a public element
+	 *
+	 */
 	public Long[] menuUuids;
-	//保存/修改
+	//Ã¤Â¿ï¿½Ã¥Â­Ëœ/Ã¤Â¿Â®Ã¦â€�Â¹
+	/**
+	 * this public element is a public element
+	 *
+	 */
 	public String save(){
 		if(rm.getUuid()== null){
 			roleEbi.save(rm,resUuids,menuUuids);
@@ -52,20 +92,24 @@ public class RoleAction extends BaseAction{
 		return TO_LIST;
 	}
 	
-	//跳转到添加/修改页面
+	//Ã¨Â·Â³Ã¨Â½Â¬Ã¥Ë†Â°Ã¦Â·Â»Ã¥Å Â /Ã¤Â¿Â®Ã¦â€�Â¹Ã©Â¡ÂµÃ©ï¿½Â¢
+	/**
+	 * this public element is a public element
+	 *
+	 */
 	public String input(){
-		//加载所有的资源数据
+		//Ã¥Å Â Ã¨Â½Â½Ã¦â€°â‚¬Ã¦Å“â€°Ã§Å¡â€žÃ¨Âµâ€žÃ¦Âºï¿½Ã¦â€¢Â°Ã¦ï¿½Â®
 		List<ResModel> resList = resEbi.getAll();
 		put("resList",resList);
 		
-		//加载所有的菜单数据
+		//Ã¥Å Â Ã¨Â½Â½Ã¦â€°â‚¬Ã¦Å“â€°Ã§Å¡â€žÃ¨ï¿½Å“Ã¥ï¿½â€¢Ã¦â€¢Â°Ã¦ï¿½Â®
 		List<MenuModel> menuList = menuEbi.getAll();
 		put("menuList",menuList);
 		
 		if(rm.getUuid()!=null){
 			rm = roleEbi.get(rm.getUuid());
-			//将rm对象中的reses转换为页面可以接收的数据格式 resUuids数组
-			//集合Set->Long[]
+			//Ã¥Â°â€ rmÃ¥Â¯Â¹Ã¨Â±Â¡Ã¤Â¸Â­Ã§Å¡â€žresesÃ¨Â½Â¬Ã¦ï¿½Â¢Ã¤Â¸ÂºÃ©Â¡ÂµÃ©ï¿½Â¢Ã¥ï¿½Â¯Ã¤Â»Â¥Ã¦Å½Â¥Ã¦â€�Â¶Ã§Å¡â€žÃ¦â€¢Â°Ã¦ï¿½Â®Ã¦Â Â¼Ã¥Â¼ï¿½ resUuidsÃ¦â€¢Â°Ã§Â»â€ž
+			//Ã©â€ºâ€ Ã¥ï¿½Ë†Set->Long[]
 			resUuids = new Long[rm.getReses().size()];
 			int i = 0;
 			for(ResModel temp: rm.getReses()){
@@ -81,7 +125,11 @@ public class RoleAction extends BaseAction{
 		return INPUT;
 	}
 
-	//删除
+	//Ã¥Ë†Â Ã©â„¢Â¤
+	/**
+	 * this public element is a public element
+	 *
+	 */
 	public String delete(){
 		roleEbi.delete(rm);
 		return TO_LIST;
