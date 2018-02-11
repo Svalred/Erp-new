39934f67bcc9d289136458c09ffafe1add4d450e
Svalred <34680485+Svalred@users.noreply.github.com>
2018-02-11 21:11:59 +0100

    Update SupplierAction.java

--- src/cn/itcast/invoice/invoice/supplier/web/SupplierAction.java
+++ src/cn/itcast/invoice/invoice/supplier/web/SupplierAction.java
@@ -6,9 +6,20 @@ import cn.itcast.invoice.invoice.supplier.business.ebi.SupplierEbi;
 import cn.itcast.invoice.invoice.supplier.vo.SupplierModel;
 import cn.itcast.invoice.invoice.supplier.vo.SupplierQueryModel;
 import cn.itcast.invoice.util.base.BaseAction;
-
+/**
+ * this class extends BaseAction
+ *
+ */
 public class SupplierAction extends BaseAction{
+	/**
+	 * this public element is a public element
+	 *
+	 */
 	public SupplierModel sm = new SupplierModel();
+	/**
+	 * this public element is a public element
+	 *
+	 */
 	public SupplierQueryModel sqm = new SupplierQueryModel();
 
 	private SupplierEbi supplierEbi;
@@ -16,7 +27,11 @@ public class SupplierAction extends BaseAction{
 		this.supplierEbi = supplierEbi;
 	}
 
-	//跳转到列表页面
+	//Ã¨Â·Â³Ã¨Â½Â¬Ã¥Ë†Â°Ã¥Ë†â€”Ã¨Â¡Â¨Ã©Â¡ÂµÃ©ï¿½Â¢
+	/**
+	 * this public element is a public element
+	 *
+	 */
 	public String list(){
 		setDataTotal(supplierEbi.getCount(sqm));
 		List<SupplierModel> supplierList = supplierEbi.getAll(sqm,pageNum,pageCount);
@@ -24,7 +39,11 @@ public class SupplierAction extends BaseAction{
 		return LIST;
 	}
 
-	//保存/修改
+	//Ã¤Â¿ï¿½Ã¥Â­Ëœ/Ã¤Â¿Â®Ã¦â€�Â¹
+	/**
+	 * this public element is a public element
+	 *
+	 */
 	public String save(){
 		if(sm.getUuid()== null){
 			supplierEbi.save(sm);
@@ -34,7 +53,11 @@ public class SupplierAction extends BaseAction{
 		return TO_LIST;
 	}
 
-	//跳转到添加/修改页面
+	//Ã¨Â·Â³Ã¨Â½Â¬Ã¥Ë†Â°Ã¦Â·Â»Ã¥Å Â /Ã¤Â¿Â®Ã¦â€�Â¹Ã©Â¡ÂµÃ©ï¿½Â¢
+	/**
+	 * this public element is a public element
+	 *
+	 */
 	public String input(){
 		if(sm.getUuid()!=null){
 			sm = supplierEbi.get(sm.getUuid());
@@ -42,7 +65,11 @@ public class SupplierAction extends BaseAction{
 		return INPUT;
 	}
 
-	//删除
+	//Ã¥Ë†Â Ã©â„¢Â¤
+	/**
+	 * this public element is a public element
+	 *
+	 */
 	public String delete(){
 		supplierEbi.delete(sm);
 		return TO_LIST;
